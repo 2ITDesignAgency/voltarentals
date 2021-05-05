5079a4834dc8c3bb56c21de25810b796d2ccdda0
Khatchig Garabedian <32610134+2ITDesignAgency@users.noreply.github.com>
2021-05-05 16:22:46 +0300

    chenges in node

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,11 @@ module.exports = {
       options: {
         url: process.env.WPGRAPHQL_URL,
         verbose: true,
+        schema: {
+          perPage: 50,
+          requestConcurrency: 5,
+          previewRequestConcurrency: 2,
+        },
         develop: {
           hardCacheMediaFiles: true,
         },
@@ -52,7 +57,7 @@ module.exports = {
             limit:
               process.env.NODE_ENV === `development`
                 ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  200
+                  50
                 : // And then we can pull all posts in production
                   null,
           },
