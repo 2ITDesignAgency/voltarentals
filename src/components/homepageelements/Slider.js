09fe1fe6c9a11b61c67bdaa2128ab6c32a098cdd
Khatchig Garabedian <32610134+2ITDesignAgency@users.noreply.github.com>
2021-09-30 11:01:16 +0300

    removed text from slider

--- src/components/homepageelements/Slider.js
+++ src/components/homepageelements/Slider.js
@@ -2,14 +2,12 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import BackgroundSlider from "gatsby-image-background-slider"
 
-
 const SliderHome = ({ children }) => (
-  
   <>
     <BackgroundSlider
       width="1920px"
       height="400px"
-      query ={useStaticQuery(graphql`
+      query={useStaticQuery(graphql`
         query {
           backgrounds: allFile(
             filter: { sourceInstanceName: { eq: "backgrounds" } }
@@ -25,24 +23,34 @@ const SliderHome = ({ children }) => (
           }
         }
       `)}
-      
       initDelay={2} // delay before the first transition (if left at 0, the first image will be skipped initially)
       transition={4} // transition duration between images
       duration={8} // how long an image is shown
       // specify images to include (and their order) according to `relativePath`
       images={["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]}
     >
-     {/* Captions in sync with background images*/}
-      <div>Woof</div>
-      <div>Meow</div>
-      <>{/* Giraffes don't talk; [they aren't real](https://chivomengro.com/2017/10/23/the-truth-comes-out-giraffes-are-a-hoax/) */}</>
-      <a href="https://en.wikipedia.org/wiki/Tasmanian_devil#Conservation_status">
-        I could use a hand
-      </a>
-      <div>I need to find better hobbies</div>
+      {/* Captions in sync with background images*/}
+      {/* <div className="slider-text" style="text-align: center;">
+        <h3>lorem ispum1</h3>
+        <p>lorem ipsum subtitle</p>
+      </div>
+      <div className="slider-text"> 
+        <h3>lorem ispum2</h3>
+        <p>lorem ipsum subtitle</p>
+        </div>
+      <div className="slider-text">
+      <h3>lorem ispum3</h3>
+      <p>lorem ipsum subtitle</p>
+      </div>
+      <div className="slider-text">
+      <h4>lorem ispum4</h4>
+      <p>lorem ipsum subtitle</p>        
+      </div>
+      <div className="slider-text">
+      <h4>lorem ispum5</h4>
+      <p>lorem ipsum subtitle</p>        
+      </div> */}
     </BackgroundSlider>
-      
- 
   </>
 )
 export default SliderHome
