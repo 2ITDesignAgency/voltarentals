d945fb9da95fc914f958839fb51a618fe0643ec9
Khatchig Garabedian <32610134+2ITDesignAgency@users.noreply.github.com>
2021-10-11 14:22:58 +0300

    changed pull 30 post

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
             limit:
               process.env.NODE_ENV === `development`
                 ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  50
+                  30
                 : // And then we can pull all posts in production
                   null,
           },
